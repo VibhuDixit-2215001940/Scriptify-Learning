fb7857bba9e45d0e71937a79e80e9d6f53a124b9
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-03-04 15:35:01 +0530

    add Till process

---
+++ Lec-42(process,argv,cwd,__dirname)/app.js
@@ -0,0 +1,25 @@
+// let pi = 3.145;
+// console.log(pi)
+// console.log("Hello")
+
+
+
+// console.log(process.argv)
+
+// let inputs = process.argv.slice(2);
+// console.log(inputs)
+
+
+
+//-----------TASK
+// let input = process.argv.slice(2);
+// console.log(input)
+// let n = input[0];
+// let m = input[1];
+// for(let i = n; i <= m; i++){
+//     console.log(i)
+// }
+
+
+//---------------------
+console.log(process.cwd())
